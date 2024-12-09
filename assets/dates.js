573fc34930fd89cd371752074c1b43ce50d6dd38
valkyrie_pilot <valk@randomairborne.dev>
2024-12-08 23:26:26 -0700

    i missed a console.log, quick disguise this as a prettier commit

--- assets/dates.js
+++ assets/dates.js
@@ -1,5 +1,4 @@
 document.querySelectorAll("[data-timestamp]").forEach((elm) => {
-    console.log(elm)
   elm.innerText = new Date(
     parseInt(elm.dataset.timestamp) * 1000,
   ).toLocaleString();
